8b2e5a62cc6257f22f5fdca75995a7085c0bdb52
Javier Ramila <jandramila@gmail.com>
2019-06-25 20:49:51 -0300

    Adds sample document to App initial state.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,5 +1,14 @@
 class DocumentsController < ApplicationController
   def show
-    render_react_app :documents
+    sample_document = Document.includes(:pages, pages: [:checkboxes]).first
+    pages = sample_document.pages.map do |page|
+      page.as_json.merge(checkboxes: page.checkboxes)
+    end
+
+    render_react_app :documents,
+      document: {
+        id: sample_document.id,
+        pages: pages,
+      }
   end
 end
